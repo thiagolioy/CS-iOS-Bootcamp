b65e19675b181ac3bbcc960f946d4694028250c9
Jean Vinge <jeanvinge@MacBook-Pro-de-SUPPORT.local>
2015-08-24 11:14:09 -0300

    adding client class

---
+++ Wattpad-app/Client.h
@@ -0,0 +1,15 @@
+//
+//  Client.h
+//  Wattpad-app
+//
+//  Created by Jean Vinge on 24/08/15.
+//  Copyright (c) 2015 Jean Vinge. All rights reserved.
+//
+
+#import <AFNetworking/AFHTTPRequestOperationManager.h>
+
+@interface Client : AFHTTPRequestOperationManager
+
++ (instancetype)sharedInstance;
+
+@end
